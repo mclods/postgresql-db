00e6dbcd27092942238033abfb952deec8a7e209
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2026-04-06 00:22:35 +0530

    Solved Problem

---
+++ leetcode_db/CombineTwoTables.sql
@@ -0,0 +1,16 @@
+-- https://leetcode.com/problems/combine-two-tables/description/
+-- Schema
+Create table If Not Exists Person (personId int, firstName varchar(255), lastName varchar(255))
+Create table If Not Exists Address (addressId int, personId int, city varchar(255), state varchar(255))
+Truncate table Person
+insert into Person (personId, lastName, firstName) values ('1', 'Wang', 'Allen')
+insert into Person (personId, lastName, firstName) values ('2', 'Alice', 'Bob')
+Truncate table Address
+insert into Address (addressId, personId, city, state) values ('1', '2', 'New York City', 'New York')
+insert into Address (addressId, personId, city, state) values ('2', '3', 'Leetcode', 'California')
+
+-- Query
+SELECT person.firstname AS firstName, person.lastname AS lastName, address.city, address.state
+FROM person
+LEFT JOIN address
+ON person.personid = address.personid;
