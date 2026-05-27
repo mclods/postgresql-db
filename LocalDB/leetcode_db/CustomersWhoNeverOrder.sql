6c826550d7dc8570e844ca052b3685e1ff9f23b2
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2026-05-27 19:05:51 +0530

    Solved Problem

---
+++ LocalDB/leetcode_db/CustomersWhoNeverOrder.sql
@@ -0,0 +1,22 @@
+-- https://leetcode.com/problems/customers-who-never-order/description/
+
+DROP TABLE IF EXISTS Customers;
+DROP TABLE IF EXISTS Orders;
+
+Create table If Not Exists Customers (id int, name varchar(255));
+Create table If Not Exists Orders (id int, customerId int);
+Truncate table Customers;
+insert into Customers (id, name) values ('1', 'Joe');
+insert into Customers (id, name) values ('2', 'Henry');
+insert into Customers (id, name) values ('3', 'Sam');
+insert into Customers (id, name) values ('4', 'Max');
+Truncate table Orders;
+insert into Orders (id, customerId) values ('1', '3');
+insert into Orders (id, customerId) values ('2', '1');
+
+
+SELECT c.name AS customers
+FROM customers c
+LEFT JOIN orders o
+ON c.id = o.customerId
+WHERE o.id IS NULL;
